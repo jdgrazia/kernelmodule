f385a18e9f190a68b3be5bbd099ba0b0845ec647
jdgrazia <josephgraziani@gmail.com>
2017-05-06 05:27:56 +0000

    finished first hello

---
+++ hello-1.c
@@ -0,0 +1,13 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+int init_module(void){
+
+	printk(KERN_INFO "fuck off");
+	return 0;
+}
+
+int cleanup_module(void){
+
+	printk(KERN_INFO "I'm out");
+}
