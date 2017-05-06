f452258ba6da66c21435e07759767b3784dc3279
jdgrazia <josephgraziani@gmail.com>
2017-05-06 19:07:35 +0000

    used init.h to bring in macros and change func names

--- hello-1.c
+++ hello-1.c
@@ -1,13 +1,21 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
 
-int init_module(void){
+
+MODULE_LICENSE("GPL");
+
+static int __init in(void){
 
 	printk(KERN_INFO "fuck off");
 	return 0;
 }
 
-int cleanup_module(void){
+static void __exit out(void){
 
 	printk(KERN_INFO "I'm out bitches");
 }
+
+module_init(in);
+module_exit(out);
+
